fcc454ef937fc4bfb09b5d19b622ac8951e7ce4f
Tianyu Liu <tianyuliu@college.harvard.edu>
2015-04-03 03:05:09 -0400

    Add basic file depend tree

--- kern/arch/mips/vm/vm.c
+++ kern/arch/mips/vm/vm.c
@@ -1,1 +1,12 @@
+#include <types.h>
+#include <kern/errno.h>
+#include <lib.h>
+#include <spl.h>
+#include <spinlock.h>
+#include <proc.h>
+#include <current.h>
+#include <mips/tlb.h>
+#include <addrspace.h>
+#include <vm.h>
+#include <coremap.h>
 // TODO: vm code goes here
--- kern/vm/paging.c
+++ kern/vm/paging.c
@@ -1,1 +1,10 @@
+#include <types.h>
+#include <kern/errno.h>
+#include <lib.h>
+#include <setjmp.h>
+#include <thread.h>
+#include <current.h>
+#include <vm.h>
+#include <copyinout.h>
+#include <paging.h>
 // TODO: paging code goes here
